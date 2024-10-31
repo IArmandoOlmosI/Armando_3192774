7f47125c3efd9f92eba5bf5e76f71095706583e4
IArmandoOlmosI <armandojos3204@gmail.com>
2024-10-30 23:26:33 -0600

    Subiendo rutas con express

--- nodeExpress/servidor.js
+++ nodeExpress/servidor.js
@@ -2,25 +2,27 @@
 
 const express = require("express");
 const path = require("path");
+const pagesRoutes = require("./routes/pages");
 
 const app = express();
-const port = 3009;
+const port = 3025;
 
 
 app.use(express.static('public'));
+app.use('/',pagesRoutes);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'principal.html'));
+app.get('/',(req,res)=>{
+    res.redirect('/principal');
 });
 
-app.get('/formulario', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'formulario.html'));
+app.get('/',(req,res)=>{
+    res.redirect('/generador');
 });
 
-app.get('/informacion', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'informacion.html'));
+app.get('/',(req,res)=>{
+    res.redirect('/calculadora');
 });
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(port, ()=>{
+    console.log(`http://localhost:${port}`);
 });
